ea07c9a432a75d74547f30114c0caae663c01837
jerry <Jeremiahjacob261@gmail.com>
2025-03-09 11:52:34 +0100

    fix errir - font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Cormorant_Garamond ,Poppins} from "next/font/google";
+import { Geist, Geist_Mono ,Poppins} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,11 +10,6 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
-const cormorantGaramond = Cormorant_Garamond({
-  variable: "--font-cormorant-garamond",
-  subsets: ["latin"],
-  weight:['400', '500', '600', '700'],
 });
 
 const poppins = Poppins({
