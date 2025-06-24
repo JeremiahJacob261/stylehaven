5aa2d4660efe843b4554e365736ddaf4b51f421a
jerry <Jeremiahjacob261@gmail.com>
2025-06-24 08:03:11 +0100

    swicths

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { AuthProvider } from "@/contexts/AuthContext";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
-
+ const PAYPAL_C_ID = process.env.PAYPAL_CLIENT_ID;
 export const metadata: Metadata = {
   title: "NateTube Receipt Generator",
   description: "Generate receipts to match your wins",
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script src="https://www.paypal.com/sdk/js?client-id=AQz7RYMMZ0Hz6Er_rfkS8O1or9b0wF2UXJR2P50yQeMt2o0ZmqDy40VCV8yMn0_iic26yiXFavL7lGFz&currency=USD" />
+        <Script src={`https://www.paypal.com/sdk/js?client-id=${PAYPAL_C_ID}&currency=USD`} />
       </head>
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
